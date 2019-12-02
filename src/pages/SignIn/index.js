6aea0a0fbef6f412de61d30362b036bee8165f7c
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-02 15:51:25 -0300

    Created Component Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 
-import {
-  Container,
-  Form,
-  FormInput,
-  Image,
-  SubmitButton,
-  SubmitButtonText,
-} from './styles';
+import { Container, Form, FormInput, Image, SubmitButton } from './styles';
 
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return (
-    <Container>
-      <Image source={logo} />
-      <Form>
-        <FormInput
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Informe seu ID de cadastro"
-        />
-        {/* <SubmitButton onPress={() => {}}>
-          <SubmitButtonText>Entrar no sistema</SubmitButtonText>
-        </SubmitButton> */}
-      </Form>
-    </Container>
-  );
+    return (
+        <Container>
+            <Image source={logo} />
+            <Form>
+                <FormInput
+                    keyboardType="email-address"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Informe seu ID de cadastro"
+                />
+                <SubmitButton>Entrar no Sistema</SubmitButton>
+            </Form>
+        </Container>
+    );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,32 +1,33 @@
 import styled from 'styled-components/native';
+import Button from '~/components/Button';
 
 export const Container = styled.View`
-  background: #ffffff;
-  flex: 1px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 25px;
+    background: #ffffff;
+    flex: 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 25px;
 `;
 
 export const Image = styled.Image`
-  width: 122px;
-  height: 80px;
+    width: 122px;
+    height: 80px;
 `;
 
 export const Form = styled.View`
-  width: 100%;
+    width: 100%;
 `;
 
 export const FormInput = styled.TextInput`
-  font-size: 15px;
-  margin-top: 20px;
-  color: #999999;
-  padding: 10px 20px;
-  border: 1px solid #dddddd;
-  border-radius: 4px;
+    font-size: 15px;
+    margin-top: 20px;
+    color: #999999;
+    padding: 10px 20px;
+    border: 1px solid #dddddd;
+    border-radius: 4px;
 `;
 
-export const SubmitButton = styled.TouchableNativeFeedback``;
-
-export const SubmitButtonText = styled.Text``;
+export const SubmitButton = styled(Button)`
+    margin-top: 20px;
+`;
