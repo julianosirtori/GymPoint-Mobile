75ffb6d58d29a25f84cd03a9179e9524e3bc3cc1
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-02 16:10:04 -0300

    Created component Card

---
+++ src/components/Card/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    flex-direction: column;
+    background: #ffffff;
+    border: 1px solid #dddddd;
+    padding: 20px;
+    border-radius: 4px;
+`;
