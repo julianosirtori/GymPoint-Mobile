09386eb0927052820faf47338df1205900fca982
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-11 06:04:56 -0300

    Add persist reducers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 
 import './config/ReactotronConfig';
-import { store } from './store';
+import { store, persistor } from './store';
 
 import App from './App';
 
 export default function Index() {
     return (
         <Provider store={store}>
-            <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-            <App />
+            <PersistGate persistor={persistor}>
+                <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+                <App />
+            </PersistGate>
         </Provider>
     );
 }
