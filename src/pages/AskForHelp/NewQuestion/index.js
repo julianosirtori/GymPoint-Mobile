175dd5973cce8e25d3ccfc8470f7c253a607c2c8
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-14 14:21:24 -0300

    Create New Question

--- src/pages/AskForHelp/NewQuestion/index.js
+++ src/pages/AskForHelp/NewQuestion/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Alert } from 'react-native';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import Button from '~/components/Button';
+
+import { Container, Form, TextArea } from './styles';
 
 export default function NewQuestion() {
-    return <View />;
+    const student = useSelector(state => state.auth.student);
+    const [question, setQuestion] = useState('');
+
+    function handleQuestion(value) {
+        setQuestion(value);
+    }
+
+    async function submit() {
+        await api.post(`/students/${student.id}/help-orders`, { question });
+        Alert.alert(
+            'Sucesso !',
+            'Seu pedido de auxilio foi salvo com sucesso !'
+        );
+        setQuestion('');
+    }
+
+    return (
+        <Container>
+            <Form>
+                <TextArea
+                    multiline
+                    placeholder="Inclua seu pedido de auxílio"
+                    numberOfLines={15}
+                    onChangeText={handleQuestion}
+                    textAlignVertical="top"
+                    onSubmitEditing={submit}
+                    value={question}
+                />
+                <Button onPress={submit}>Enviar pedido</Button>
+            </Form>
+        </Container>
+    );
 }
---
+++ src/pages/AskForHelp/NewQuestion/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    padding: 20px;
+`;
+
+export const Form = styled.View``;
+
+export const TextArea = styled.TextInput`
+    padding: 20px;
+    background: #ffffff;
+    margin-bottom: 20px;
+    border: 1px solid #dddddd;
+    border-radius: 4px;
+    font-size: 16px;
+    color: #999999;
+`;
